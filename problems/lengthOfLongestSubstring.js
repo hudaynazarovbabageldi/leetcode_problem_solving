87bdacadb859ef888683c0a6d93d4c01006adab9
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-07-24 02:22:22 +0500

    [Update] lengthOfSubstring problem solved

--- problems/lengthOfLongestSubstring.js
+++ problems/lengthOfLongestSubstring.js
@@ -3,26 +3,21 @@
  * @return {number}
  */
 
-// Example 1:
-
-// Input: s = "abcabcbb"
-// Output: 3
-// Explanation: The answer is "abc", with the length of 3.
-// Example 2:
-
-// Input: s = "bbbbb"
-// Output: 1
-// Explanation: The answer is "b", with the length of 1.
-// Example 3:
-
-// Input: s = "pwwkew"
-// Output: 3
-// Explanation: The answer is "wke", with the length of 3.
-// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
 var lengthOfLongestSubstring = function (s) {
-  let longest = 0;
-  let obj = {};
-  for (let el of s) {
+  let set = new Set();
+  let left = 0;
+  let maxLength = 0;
+
+  for (let right = 0; right < s.length; right++) {
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
+    }
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
   }
+  console.log("set: ", set);
+  return maxLength;
 };
+
+console.log(lengthOfLongestSubstring("pwwkew"));
