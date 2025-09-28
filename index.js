d29f3cd55fe64385dc7c2c57199ffd4cfef755cd
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-09-29 04:11:30 +0500

    [chore] Solved debounce and letterCombination

---
+++ index.js
@@ -0,0 +1,8 @@
+let arr = [12, 12];
+let arr1 = [12, 12];
+
+const a = 12;
+const b = 12;
+
+// console.log(arr === arr1);
+console.log(a === b);
---
+++ problems/letterCombinations.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+const buttons = {
+  1: "",
+  2: "abc",
+  3: "def",
+  4: "ghi",
+  5: "jkl",
+  6: "mno",
+  7: "pqrs",
+  8: "tuv",
+  9: "wxyz",
+};
+
+var letterCombinations = function (digits) {
+  let length = digits.length;
+  let str = "";
+  for(let i = 0; i < dig)
+};
+
+console.log(letterCombinations("digit"));
