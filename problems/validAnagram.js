282073014d8e67efa402308d9370335134f8d38a
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-10-26 10:13:28 +0500

    feat: Added new Solution to validAnagram with O(n) time complexity.(I love hashmap)

--- problems/validAnagram.js
+++ problems/validAnagram.js
@@ -1,24 +1,43 @@
+/// first O(n log n) solution
+// /**
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// var isAnagram = function (s, t) {
+//   let s1 = s.split("");
+//   let t1 = t.split("");
+
+//   s1 = s1.sort();
+//   t1 = t1.sort();
+
+//   const s2 = s1.join("");
+//   const t2 = t1.join("");
+
+//   return s2 === t2;
+// };
+
+// console.log("isAnagram: ", isAnagram("anagram", "nagaram"));
+
+// second O(n) solution
+
 /**
  * @param {string} s
  * @param {string} t
  * @return {boolean}
  */
 var isAnagram = function (s, t) {
-  let s1 = s.split("");
-  let t1 = t.split("");
+  if (s.length !== t.length) return false;
 
-  s1 = s1.sort();
-  t1 = t1.sort();
+  let count = {};
 
-  const s2 = s1.join("");
-  const t2 = t1.join("");
+  for (let char of s) {
+    count[char] = (count[char] || 0) + 1;
+  }
 
-  return s2 === t2;
+  for (let char of t) {
+    if (!count[char]) return false;
+    count[char]--;
+  }
+  return true;
 };
-
-console.log("isAnagram: ", isAnagram("anagram", "nagaram"));
-
-// const arr = ["a", "b", "c"];
-// const arr2 = ["a", "b", "c"];
-
-// console.log(arr.join(""));
