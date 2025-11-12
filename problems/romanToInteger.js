b5d9ff37fd15766c79324bfcbe8b8e5b173df99c
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-11-12 22:05:31 +0500

    feat: Added romanToInteger.js problem

---
+++ problems/romanToInteger.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+  const map = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    const curr = map[s[i]];
+    const next = map[s[i + 1]];
+
+    if (curr < next) {
+      total = total - curr;
+    } else {
+      total = total + curr;
+    }
+  }
+
+  return total;
+};
+
+console.log("romanToInt: ", romanToInt("MCMXCIV"));
