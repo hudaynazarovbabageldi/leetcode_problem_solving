7e6d15ebf22431ab17a4b453a18316f402f388d7
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-11-22 20:22:17 +0500

    feat: Added second sol of intersection(using set)

--- problems/intersection.js
+++ problems/intersection.js
@@ -1,27 +1,28 @@
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-var intersection = function (nums1, nums2) {
-  let map = {};
-  let intersection = [];
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+// var intersection = function (nums1, nums2) {
+//   let map = {};
+//   let intersection = [];
 
-  for (let el of nums1) {
-    map[el] = true;
-  }
+//   for (let el of nums1) {
+//     map[el] = true;
+//   }
 
-  for (let el of nums2) {
-    if (map[el]) {
-      intersection.push(el);
-      map[el] = false;
-    }
-  }
+//   for (let el of nums2) {
+//     if (map[el]) {
+//       intersection.push(el);
+//       map[el] = false;
+//     }
+//   }
 
-  return intersection;
-};
+//   return intersection;
+// };
 
-console.log(intersection([1, 2, 2, 1], [2, 2]));
+// console.log(intersection([1, 2, 2, 1], [2, 2]));
+
 // console.log(intersection([9, 4, 9, 8, 4], [4, 9, 5]));
 
 // Example 1:c;e
@@ -33,3 +34,18 @@ console.log(intersection([1, 2, 2, 1], [2, 2]));
 // Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
 // Output: [9,4]
 // Explanation: [4,9] is also accepted.
+
+// Second Solution using set data structure
+
+var intersection = function (nums1, nums2) {
+  const set1 = new Set(nums1);
+  const result = new Set();
+
+  for (let el of nums2) {
+    if (set1.has(el)) {
+      result.add(el);
+    }
+  }
+
+  return [...result];
+};
