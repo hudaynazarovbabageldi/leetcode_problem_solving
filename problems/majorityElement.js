5b389bcbb23f1a695374a9fbd9d3f9d5dbf9f956
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2025-11-22 17:05:32 +0500

    feat: Added majorityElement problem

---
+++ problems/majorityElement.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+  const map = {};
+  for (let el of nums) {
+    if (map[el]) {
+      map[el] += 1;
+    } else {
+      map[el] = 1;
+    }
+  }
+  let majorityCount = 0;
+  let majorityElement = nums[0];
+  for (let el in map) {
+    if (map[el] > majorityCount) {
+      majorityCount = map[el];
+      majorityElement = parseInt(el);
+    }
+  }
+
+  return majorityElement;
+  //   return Object.keys(map).reduce((a, b) => (map[a] > map[b] ? a : b));
+};
+
+let nums = [2, 2, 1, 1, 1, 2, 2];
+console.log("hey: ", majorityElement(nums));
