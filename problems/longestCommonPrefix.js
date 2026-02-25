f8ccebb12dadb2693b3d2174d63e884683f1e724
hudaynazarovbabageldi <hudaynazarovbabageldi1@gmail.com>
2026-02-25 22:23:44 +0500

    feat: Added longestCommonPrefix

--- problems/longestCommonPrefix.js
+++ problems/longestCommonPrefix.js
@@ -1,23 +1,28 @@
-// /**
-//  * @param {string[]} strs
-//  * @return {string}
-//  */
-// var longestCommonPrefix = function (strs) {
-//   let commonPrefix = strs[0];
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  if (!strs.length) return "";
 
-//   for (let str of strs) {
-//       for (let el of str) {
-//         if()
-//       }
-//   }
-// };
+  let common = strs[0];
 
-// // Example 1:
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(common) !== 0) {
+      common = common.slice(0, common.length - 1);
+      if (common === "") return "";
+    }
+  }
 
-// // Input: strs = ["flower","flow","flight"]
-// // Output: "fl"
-// // Example 2:
+  return common;
+};
 
-// // Input: strs = ["dog","racecar","car"]
-// // Output: ""
-// // Explanation: There is no common prefix among the input strings.
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
